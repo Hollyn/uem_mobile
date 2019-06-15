c0314890a03523c2f92a4e36fe02e4cd824bd5cd
hollyn <hollyn.derisse@esih.edu>
2019-06-15 19:01:38 +0700

    Initial commit

---
+++ University Events Management/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  University Events Management
+//
+//  Created by Hollyn Derisse on 6/15/19.
+//  Copyright © 2019 Hollyn Derisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
