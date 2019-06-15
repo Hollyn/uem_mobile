bb25a025e1ac152dbce7f8733aa565363b1bcf8f
hollyn <hollyn.derisse@esih.edu>
2019-06-15 20:47:13 +0700

    first version of the login

--- University Events Management/ViewController.swift
+++ University Events Management/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // properties
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func bgOutside(_ sender: UITapGestureRecognizer) {
+        
+        emailField.resignFirstResponder()
+            passwordField.resignFirstResponder()
+    }
+    
 }
 
